076b5dccdafedb1c86976878cc2ad599e8196a14
snaydersonly <didimariadi.hazanatun@gmail.com>
2025-02-01 18:59:13 +0700

    Update fix-all.sh

--- fix-all.sh
+++ fix-all.sh
@@ -10,3 +10,6 @@ END
 service cron restart
 service cron reload
 systemctl restart cron
+
+wget -q -O /usr/local/share/xray/geosite.dat "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat" >/dev/null 2>&1
+wget -q -O /usr/local/share/xray/geoip.dat "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat" >/dev/null 2>&1
